571184410d3598a041ca9086c4215436f32ca280
John Codeinaire <johncodeinaire@gmail.com>
2020-05-29 11:43:10 +1000

    GTG - fixed issues and whole workflow test  A-OK

--- api/index.js
+++ api/index.js
@@ -63,13 +63,13 @@ module.exports = async (req, res) => {
     })
 
     //* 6) Send mail with defined transport object
-    const companyName = enrolleeInfoForFirmex[0].Company
+    const companyName = enrolleeInfoForFirmex[0].company
     const emailRes = await transporter.sendMail({
       from: `'Priority Management Sydney' <brett.handley@prioritymanagement.com.au>`,
       to: 'john.stewart@ethicaltechnology.co',
       subject: 'Firmex spreadsheet',
-      text: `Dear Sandi,/r This is the Firmex spreadsheet containing users with ${companyName}/r Regards,/rAdriana Parinetto`,
-      html: `<p>Dear Sandi,</p><p>This is the Firmex spreadsheet containing users with ${companyName}</p><p>Regards,<br/>Adriana Parinetto</p>`,
+      text: `Dear Sandi,/r This is the Firmex spreadsheet containing users from ${companyName}/r Regards,/rAdriana Parinetto`,
+      html: `<p>Dear Sandi,</p><p>This is the Firmex spreadsheet containing users from ${companyName}</p><p>Regards,<br/>Adriana Parinetto</p>`,
       attachments: [
         {
           filename: 'firmexSpreedsheet.xlsx',
